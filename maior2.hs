abcb7da34598707ea18fed82fdaf757420a757c3
Willian Lauber <lcwillian_hd@hotmail.com>
2016-11-29 20:07:32 -0200

    Update maior2.hs

--- maior2.hs
+++ maior2.hs
@@ -2,6 +2,8 @@
 maior2 a b =
    if a>b then a else b  
    
-maior2_guarda a b
-	| a>b = a
-	| otherwise = b
+maior2_guarda a b 	| a>b = a  	| otherwise = b
+	
+	
+maior3_if a b c = if(a>=b && a>=c) then a else if(b>=a && b>=c) then b else c
+
